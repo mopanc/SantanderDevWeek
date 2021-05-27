7bfb3212859698399ad0f63ee30cd6e6a9514690
mopanc <jorgemopanc@icloud.com>
2021-05-26 23:17:22 -0300

    criando o layout

--- app/src/main/java/com/mopanc/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/mopanc/santanderdevweek/MainActivity.kt
@@ -2,11 +2,15 @@ package com.mopanc.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         // declarndo variaveis
         // val = constante
@@ -35,5 +39,21 @@ class MainActivity : AppCompatActivity() {
         //}
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d("CLICK", "Click no item 1")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
